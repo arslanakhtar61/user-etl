9bfb7cc010f2ea2fca393c5aa9ef7a3afe30fe85
Arslan Akhtar <arslanakhtar61@gmail.com>
2020-10-10 02:59:06 +0800

    ignore failed test

--- src/main/java/com/handsonconnect/useretl/model/NGO.java
+++ src/main/java/com/handsonconnect/useretl/model/NGO.java
@@ -21,6 +21,28 @@ public class NGO {
     @JoinColumn(name = "impact_area_id", referencedColumnName = "impact_area_id")
     private ImpactArea impactArea;
 
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "age")
+    private int age;
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
     public Integer getNgo_id() {
         return ngo_id;
     }
--- src/test/java/com/handsonconnect/useretl/controller/NGOControllerTest.java
+++ src/test/java/com/handsonconnect/useretl/controller/NGOControllerTest.java
@@ -12,6 +12,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTestContextBootstrapper;
+import org.springframework.boot.test.context.SpringBootTestContextBootstrapper;
 import org.springframework.boot.test.json.JacksonTester;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
