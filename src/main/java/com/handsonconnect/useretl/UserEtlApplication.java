00792187cadd12ac2945f822d5981e47d26ac66f
Arslan Akhtar <arslanakhtar61@gmail.com>
2020-10-09 18:02:00 +0800

    initial commit

---
+++ src/main/java/com/handsonconnect/useretl/UserEtlApplication.java
@@ -0,0 +1,13 @@
+package com.handsonconnect.useretl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserEtlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserEtlApplication.class, args);
+	}
+
+}
